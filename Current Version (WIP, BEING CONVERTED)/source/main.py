1b6611e7b9b0b8f51c9d81af1beaf42c921d4fea
Fawkes-c <dan_fawkes@protonmail.com>
2019-12-28 14:39:36 +0000

    Updated Homepage And Added The Last Few Routes To main.py
    Changes:
      - main.py
        - The last few @Application.route()s have been added to this File. It's now just a matter of adding the rest of
        the Pages to the Software. I've left a note about the Pages.
        - I've also added a documentation-string that details what the Routing does and how it works in a basic manner.
    
    Additions:
      - index.html
        - Menubar has been updated to be included in a <div> Tag. Secondly, a button for the Homepage and Upload Pages
        have been added to this Menubar.
        - There is also a new Section on the page which welcomes the User. This section details how many Packages they've
        uploaded, what Version the Site is currently running and how to contact the Administrator. Many more bits have yet
        to be added to it and of course, we've got the functionality to add to it.

--- Current Version (WIP, BEING CONVERTED)/source/main.py
+++ Current Version (WIP, BEING CONVERTED)/source/main.py
@@ -1,8 +1,8 @@
 """
      WebWareBox
-     Version: 0.1.0(Day/Month/Year) - NOTE: REPLACE THAT DATE UPON THE OFFICIAL RELEASE OF THE VERSION
+     Version: R 0001(Day/Month/Year) - NOTE: REPLACE THAT DATE UPON THE OFFICIAL RELEASE OF THE VERSION
      Developer: williamdev(William Boycher) and others on GitHub
-     Source-Code: https://www.github.com/williamdev/WebWareBox
+     Source-Code: https://www.github.com/HawkTechnology/WebWareBox
 """
 
 """
@@ -26,12 +26,47 @@ from flask import render_template # This Module gives us the ability to render H
 
 Application = Flask(__name__)
 
-# Home() should only trigger upon entering the Homepage('https://URL.net/' in this case)
+"""
+     This part of main.py revolves around Page-Redirection.
+     
+     Depending on what the Name of the Function is and what the URL is that it leads to, it's quite obvious on
+     what each one is supposed to do.
+     
+     First, we declare where it will head. So the Homepage is at 'https://URL.net/' and the Software-Page is at
+     'https://URL.net/Software'. Next, we return a function to render one of the HTML Pages in /templates/. But
+     to render it, we must tell the render_template() function, what HTML File it is that we want it to render.
+"""
+
+"""
+     Note by Daniel Fawkes(Fawkes-c) - Saturday, 28th December 2019
+     --------------------------------------------------------------
+     Please note that both the Homepage and the Source-Code Page are incomplete.
+     Second, please also note that the Software, Upload, Login and Registration Pages haven't even been created
+     yet. Issues have been filed for them to start development on them. If you can and have the time, please start
+     development on these pages.
+
+     Thanks.
+"""
 @Application.route('/')
 def Home(name=None):
-     return render_template("index.html", name=name)
+    return render_template("index.html", name=name)
     
-# Source() should only trigger upon entering the Source-Code Page('https://URL.net/Source' in this case)
 @Application.route('/Source')
 def Source(name=None):
-     return render_template("source.html", name=name)
+    return render_template("source.html", name=name)
+
+@Application.route('/Software')
+def Software(name=None):
+    return render_template("software.html", name=name)
+
+@Application.route('/Upload')
+def Upload(name=None):
+    return render_template("upload.html", name=name)
+
+@Application.route('/Login')
+def Login(name=None):
+    return render_template("login.html", name=name)
+
+@Application.route('/Register')
+def Register(name=None):
+    return render_template("register.html", name=name)
